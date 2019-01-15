a69b376d77642dfb2cd05ea28b3b51ba5dfafca3
storm37000 <6331913+storm37000@users.noreply.github.com>
2019-01-15 03:21:48 -0500

    initial testing

---
+++ EventHandler.cs
@@ -0,0 +1,83 @@
+﻿using Smod2;
+using Smod2.Events;
+using Smod2.EventHandlers;
+using System.Collections.Generic;
+
+namespace MOTD
+{
+	class EventHandler : IEventHandlerPlayerJoin, IEventHandlerWaitingForPlayers
+	{
+		private Main plugin;
+		private Dictionary<string,uint>  msglist = new Dictionary<string, uint>();
+
+		public EventHandler(Main plugin)
+		{
+			this.plugin = plugin;
+		}
+
+		private IEnumerable<string> EnclosedStrings(string s,string begin,string end)
+		{
+			int beginPos = s.IndexOf(begin, 0);
+			while (beginPos >= 0)
+			{
+				int start = beginPos + begin.Length;
+				int stop = s.IndexOf(end, start);
+				if (stop < 0)
+					yield break;
+				yield return s.Substring(start, stop - start);
+				beginPos = s.IndexOf(begin, stop + end.Length);
+			}
+		}
+
+		public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
+		{
+			msglist.Clear();
+			Dictionary<string, string> tempdict = plugin.GetConfigDict("motd_messages");
+			foreach (KeyValuePair<string, string> entry in tempdict)
+			{
+				uint time = 5;
+				if (uint.TryParse(entry.Value, out time) == false)
+				{
+					plugin.Error(entry.Value + " is not a valid integer time for message '" + entry.Key + "'!");
+					continue;
+				}
+				string tempmsg = entry.Key;
+				foreach (string config in EnclosedStrings(entry.Key,"$S[","]"))//strings
+				{
+					tempmsg = tempmsg.Replace("$S[" + config + "]", ConfigManager.Manager.Config.GetStringValue(config," - "));
+				}
+				foreach (string config in EnclosedStrings(entry.Key, "$I[", "]"))//integers
+				{
+					tempmsg = tempmsg.Replace("$I[" + config + "]", "" + ConfigManager.Manager.Config.GetIntValue(config,0));
+				}
+				foreach (string config in EnclosedStrings(entry.Key, "$F[", "]"))//floats
+				{
+					tempmsg = tempmsg.Replace("$F[" + config + "]", "" + ConfigManager.Manager.Config.GetFloatValue(config,0f));
+				}
+				foreach (string config in EnclosedStrings(entry.Key, "$B[", "]"))//bools
+				{
+					tempmsg = tempmsg.Replace("$B[" + config + "]", "" + ConfigManager.Manager.Config.GetBoolValue(config,false));
+				}
+//				foreach (string config in EnclosedStrings(entry.Key, "$SL[", "]"))//string lists
+//				{
+//					tempmsg = tempmsg.Replace("$SL[" + config + "]", "");
+//				}
+//				foreach (string config in EnclosedStrings(entry.Key, "$IL[", "]"))//integer lists
+//				{
+//					tempmsg = tempmsg.Replace("$IL[" + config + "]", "");
+//				}
+				msglist.Add(tempmsg, time);
+			}
+		}
+
+		public void OnPlayerJoin(PlayerJoinEvent ev)
+		{
+			foreach (KeyValuePair<string, uint> entry in msglist)
+			{
+				ev.Player.PersonalBroadcast(entry.Value, entry.Key, false);
+			}
+
+			//PluginManager.Manager.EnabledPlugins[1].Details;
+		}
+	}
+}
---
+++ Main.cs
@@ -0,0 +1,70 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using System.Collections.Generic;
+
+namespace MOTD
+{
+	[PluginDetails(
+		author = "storm37000",
+		name = "MOTD",
+		description = "Displays a custom message to the player whenever a player joins.",
+		id = "s37k.motd",
+		version = "0.0.0",
+		SmodMajor = 3,
+		SmodMinor = 2,
+		SmodRevision = 0
+		)]
+	class Main : Plugin
+	{
+		public override void OnDisable()
+		{
+			this.Info(this.Details.name + " has been disabled.");
+		}
+		public override void OnEnable()
+		{
+			this.Info(this.Details.name + " has been enabled.");
+			string[] hosts = { "https://storm37k.com/addons/", "http://74.91.115.126/addons/" };
+			ushort version = ushort.Parse(this.Details.version.Replace(".", string.Empty));
+			bool fail = true;
+			string errorMSG = "";
+			foreach (string host in hosts)
+			{
+				using (UnityEngine.WWW req = new UnityEngine.WWW(host + this.Details.name + ".ver"))
+				{
+					while (!req.isDone) { }
+					errorMSG = req.error;
+					if (string.IsNullOrEmpty(req.error))
+					{
+						ushort fileContentV = 0;
+						if (!ushort.TryParse(req.text, out fileContentV))
+						{
+							errorMSG = "Parse Failure";
+							continue;
+						}
+						if (fileContentV > version)
+						{
+							this.Error("Your version is out of date, please visit the Smod discord and download the newest version");
+						}
+						fail = false;
+						break;
+					}
+				}
+			}
+			if (fail)
+			{
+				this.Error("Could not fetch latest version txt: " + errorMSG);
+			}
+		}
+		
+		public override void Register()
+		{
+			// Register Events
+			this.AddEventHandlers(new EventHandler(this));
+
+			this.AddConfig(new Smod2.Config.ConfigSetting("motd_messages", new Dictionary<string,string>(), Smod2.Config.SettingType.DICTIONARY, true, "List of messages one line at a time."));
+			this.AddConfig(new Smod2.Config.ConfigSetting("motd_printplugins", true , Smod2.Config.SettingType.BOOL, true, "Print to players the list of plugins including basic info."));
+			this.AddConfig(new Smod2.Config.ConfigSetting("info_ignoredplugin", new string[]{}, Smod2.Config.SettingType.LIST, true, "Plugins to hide from being displayed.  Backwards compat with PluginInfo."));
+			this.AddConfig(new Smod2.Config.ConfigSetting("motd_ignoredplugins", new string[]{}, Smod2.Config.SettingType.LIST, true, "Plugins to hide from being displayed"));
+		}
+	}
+}
